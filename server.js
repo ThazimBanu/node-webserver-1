4b7b926cc06cf396fea40f170cb4cb418a87f680
livingstong <administrator@RudeBoys-Mac-Pro.local>
2017-01-09 17:43:24 -0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 const express = require('express');
-const hbs =require('hbs');
+const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 
 var app = express();
+
+hbs.registerPartials(__dirname+'views/partials')
 app.set('view engine','hbs');
 
-app.use(express.static(__dirname+'/public'));
+app.use((req,resp,next)=>{
+ var now = new Date().toString();
+ var log = `${now}:${req.methos}${req.url}`;
+ console.log(log);
+ fs.appendFile('server.log',log+'\n');
+ next();
+});
+
+app.use(express.static(__dirname+ '.public'));
+hbs.registerHelper('getCurrentYear',()=>{
+ return new Date().getFullYear();
+});
+
+hbs.registerHelper('scremIt', (text)=>{
+ return text.toUpperCase();
+});
 
 app.get('/',(request,response)=>{
 response.render('home.hbs',{
-    welcomeMsg: 'Welcome to Sarah Cooking class',
-    currentYear: new Date().getFullYear()
+    welcomeMsg: 'Welcome to my website',
 });
 });
 
-// app.get('/about',(request,response)=>{
-//     response.render('about.hbs',{
-//         pagetTitle: 'About Page',
-//         currentYear: new Date().getFullYear()
-//     });
-// });
+app.get('/about',(request,response)=>{
+    response.render('about.hbs',{
+        pagetTitle: 'About Page',
+    });
+});
 
 app.get('/bad',(request,response)=>{
-   var errorMessage = response.statusCode; 
-   
-   response.send(`Unable to connect to server ${errorMessage}`);
+ response.send({
+     errorMessage: 'Unable to handle request'
+ });
 });
-app.listen(3000);
+
+app.listen(port,() => {
+    console.log(`Server is up on port ${port}`);
+});
